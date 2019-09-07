ad98f32c0fca6ec5f31728c1627d53b05398de61
simar <smgaurav312@gmail.com>
2019-09-07 16:01:14 +0530

    update api

--- src/main/java/com/example/demo/api/PublishingApi.java
+++ src/main/java/com/example/demo/api/PublishingApi.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.Author;
 import com.example.demo.model.ErrorResponse;
@@ -50,5 +51,22 @@ public interface PublishingApi {
 	ResponseEntity<String> addPublishTask(@ApiParam(value = "publishing json", required = true) @Valid @RequestBody PublishTask publishTask);
 
 	
-		
+	@ApiOperation(value = "get books by author and published year", notes = "", response = Void.class, tags = {
+			"add authors API", })
+	@ApiResponses(value = { 
+			@ApiResponse(code = 201, message = "Successfully Added", response = Void.class),
+			@ApiResponse(code = 200, message = "No changes", response = Void.class),
+			@ApiResponse(code = 400, message = "Bad Request. Please see response body for more details", response = ErrorResponse.class),
+			@ApiResponse(code = 401, message = "Unauthorized", response = Void.class),
+			@ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class) })
+
+	@RequestMapping(value = "v1/getBooksByAuthorsAndpublishedYear", consumes = {
+	   "application/json" }, method = RequestMethod.PUT)
+	ResponseEntity<?> getAllBooksFromAnAuthorInYear(
+			@ApiParam(value = "author Name", required = true) @RequestParam(value = "authorName", required = true) String authorName,
+			@ApiParam(value = "return pubshingcation type - book", required = true) @RequestParam(value = "pubType", required = true) String pubType,
+			@ApiParam(value = "published Year", required = true) @RequestParam(value = "publishedYear", required = true) Integer publishedYear);
+
+	
+	
 }
--- src/main/java/com/example/demo/api/PublishingApiController.java
+++ src/main/java/com/example/demo/api/PublishingApiController.java
@@ -1,16 +1,23 @@
 package com.example.demo.api;
 
+import java.util.List;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.model.Author;
+import com.example.demo.model.Books;
 import com.example.demo.model.PublishTask;
 import com.example.demo.service.PublishingService;
+
 import io.swagger.annotations.ApiParam;
 
 /**
@@ -25,6 +32,7 @@ public class PublishingApiController implements PublishingApi {
 	@Autowired
 	PublishingService publishingService;
 
+	@Override
 	public ResponseEntity<String> addauthor(
 			@ApiParam(value = "Author json", required = true) @Valid @RequestBody Author author) {
 
@@ -38,6 +46,7 @@ public class PublishingApiController implements PublishingApi {
 		return new ResponseEntity<String>("Author added successfully", HttpStatus.OK);
 	}
 
+	@Override
 	public ResponseEntity<String> addPublishTask(
 			@ApiParam(value = "Authors json", required = true) @Valid @RequestBody PublishTask publishTask) {
 
@@ -49,6 +58,24 @@ public class PublishingApiController implements PublishingApi {
 		}
 
 		return new ResponseEntity<String>("Author added successfully", HttpStatus.OK);
+	}
+	
+	//all books from a author in an year
+	@Override
+	public ResponseEntity<?> getAllBooksFromAnAuthorInYear(
+			@ApiParam(value = "author Name", required = true) @RequestParam(value = "authorName", required = true) String authorName,
+			@ApiParam(value = "return pubshingcation type - book", required = true) @RequestParam(value = "pubType", required = true) String pubType,
+			@ApiParam(value = "published Year", required = true) @RequestParam(value = "publishedYear", required = true) Integer publishedYear) {
+
+		List<Books> booksList = null;
+		
+		if (!StringUtils.isEmpty(authorName) && !StringUtils.isEmpty(pubType) && !StringUtils.isEmpty(publishedYear)) {
+
+			booksList = publishingService.getAllBooksBasedOnAuthor(authorName, pubType, publishedYear);
+
+		}
+
+		return new ResponseEntity<List<Books>>(booksList, HttpStatus.OK);
 	}
 
 }
